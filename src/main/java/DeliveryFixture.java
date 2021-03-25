08125069af56e43bf48b8af819d5ff32ea5754a8
Amer <amer.rasheed@gmail.com>
2021-03-25 14:25:15 +0100

    OnlineElectronicShop Executed Test Warning

---
+++ src/main/java/DeliveryFixture.java
@@ -0,0 +1,22 @@
+public class DeliveryFixture {
+    private String itm;
+    private int qty;
+    private int dis;
+
+    public void setItm(String item){
+        this.itm = item;
+    }
+    public void setQuantity(int quantity){
+        this.qty = quantity;
+    }
+    public void setDistance(int distance){
+        this.dis = distance;
+    }
+
+    public String offersFreeDelivery(){
+        OnlineElectronicShop systemUnderTest = new OnlineElectronicShop(itm,qty,dis);
+        return systemUnderTest.deliveryChargeDecision();
+    }
+
+
+}
